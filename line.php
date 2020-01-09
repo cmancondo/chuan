e166174eaf138554f61b58dc6ca903ccbd7e7f75
cmancondo <59649645+cmancondo@users.noreply.github.com>
2020-01-09 15:10:05 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U9ded443f7be9f671631b9a573417de5c',
        
         'messages' => [$messages],
       ];
